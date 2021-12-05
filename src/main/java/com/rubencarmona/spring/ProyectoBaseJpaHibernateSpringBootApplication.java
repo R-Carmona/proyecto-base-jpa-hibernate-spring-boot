7e29712424ac49617d654ddc8d457a04caf79048
Rubén Carmona <carmonagarcia@gmail.com>
2021-12-05 19:37:27 +0100

    Iniciamos repositorio.

---
+++ src/main/java/com/rubencarmona/spring/ProyectoBaseJpaHibernateSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rubencarmona.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBaseJpaHibernateSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBaseJpaHibernateSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubencarmona/spring/ProyectoBaseJpaHibernateSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubencarmona.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBaseJpaHibernateSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
